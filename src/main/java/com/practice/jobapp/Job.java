c056461dc5fe4ea5c4267d0eca6e807d2a1bae5e
Rishika Aggarwal <aggarwalrishika135@gmail.com>
2026-06-14 01:35:34 +0530

    add : post endpoint for create job

--- src/main/java/com/practice/jobapp/Job.java
+++ src/main/java/com/practice/jobapp/Job.java
@@ -1,14 +1,12 @@
 package com.practice.jobapp;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Job {
     private Long id;
     private String title;
--- src/main/java/com/practice/jobapp/controller/JobAppController.java
+++ src/main/java/com/practice/jobapp/controller/JobAppController.java
@@ -2,12 +2,27 @@ package com.practice.jobapp.controller;
 
 
 import com.practice.jobapp.Job;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+
+/*
+GET /jobs: Get all jobs
+GET /jobs/{id}: Get a specific job by ID
+POST /jobs: Create a new job (request body should contain the job details)
+DELETE /jobs/{id}: Delete a specific job by ID
+PUT /jobs/{id}: Update a specific job by ID (request body should contain the updated job)
+
+Example API URLs:
+
+GET {base_url}/jobs
+GET {base_url}/jobs/1
+POST {base_url}/jobs
+DELETE {base_url}/jobs/1
+PUT {base_url}/jobs/1
+*/
 
 @RestController
 @RequestMapping("/api/v1/jobs")
@@ -21,5 +36,13 @@ public class JobAppController {
     public List<Job> getJobs() {
         return jobs;
     }
+
+    @PostMapping
+    public String addJob(@RequestBody Job job) {
+        jobs.add(job);
+        return "Job added successfully" + job.toString();
+    }
+
+
 
 }
