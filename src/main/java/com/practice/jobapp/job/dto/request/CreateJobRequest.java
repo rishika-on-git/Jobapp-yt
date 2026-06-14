b073467a99d47d7c8fc29598566a58c7d2f81fbe
Rishika Aggarwal <aggarwalrishika135@gmail.com>
2026-06-15 02:16:16 +0530

    nit : format

--- src/main/java/com/practice/jobapp/job/dto/request/CreateJobRequest.java
+++ src/main/java/com/practice/jobapp/job/dto/request/CreateJobRequest.java
@@ -1,6 +1,9 @@
 package com.practice.jobapp.job.dto.request;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
